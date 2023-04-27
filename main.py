48695d36d85d37fc513f6955057118f7bdd32c38
ofwinds <ofwinds@gmail.com>
2023-04-27 16:11:25 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,114 @@
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pymongo.mongo_client import MongoClient
+from typing import List
+
+uri = "mongodb+srv://iriscciou:supermon@selfuse.wvpvqks.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+try:
+    client.admin.command('ping')
+    db = client["Selfuse"]
+    col = db["poll"]
+except Exception as e:
+    print(e)
+
+
+# App initailization
+app = FastAPI()
+
+#templates
+templates = Jinja2Templates(directory="templates")
+app.mount("/templates", StaticFiles(directory="templates", html=True), name="templates")
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_json(message)
+
+
+manager = ConnectionManager()
+
+
+
+@app.get("/")
+async def get(request: Request):
+    votes = get_vote()
+    vote_cnt = vote_cal(votes)
+    print(vote_cnt)
+    return templates.TemplateResponse("Index.html", {'request': request, 'votes': vote_cnt})
+
+
+# recieve js value
+@app.websocket("/sendVote")
+async def user_vote(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive()
+            if data['type'] != 'websocket.disconnect':
+                if data['text'] != 'Reset':
+                    doc = {"vote": data, "num": 1}
+                    add_vote(doc)
+                else:
+                    reset_vote()
+
+            votes_now = get_vote()
+            vote_now = vote_cal(votes_now)
+            await manager.broadcast(vote_now)
+    except Exception as ex:
+        print(ex)
+
+
+def get_vote():
+    group = { "$group": {"_id": "$vote", "cnt": {"$sum": "$num"}}}
+    pipeline = [group]
+    results = col.aggregate(pipeline)
+    result_list = list(results)
+
+    return result_list
+
+def vote_cal(vote_result):
+    vote_count = [0, 0, 0, 0, 0, 0]
+    options = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+    for v in vote_result:
+        if type(v['_id']) is dict:
+            if 'text' in v['_id']:
+                vote_count[options.index(v['_id']['text'])] += v['cnt']
+
+    return vote_count
+
+def add_vote(vote):
+    col.insert_one(vote)
+
+def reset_vote():
+    col.delete_many({"num": 1})
+
+def connect_db():
+# connect to DB
+    uri = "mongodb+srv://iriscciou:supermon@selfuse.wvpvqks.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(uri)
+    try:
+        client.admin.command('ping')
+        db = client["Selfuse"]
+        col = db["poll"]
+        return col
+    except Exception as e:
+        print(e)
+
+    
+    
+    
---
+++ templates/js/chart.js
@@ -0,0 +1,78 @@
+// 準備畫圖的資料
+var ws = new WebSocket("ws://localhost:8000/sendVote");
+var Chart;
+
+function setChart(vote_cnt){
+    
+    var ctx = document.getElementById("VoteChart").getContext('2d'); 
+    var chartData = 
+{
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+}
+    var myChart = new Chart(ctx, chartData);
+    myChart.data.datasets[0].data = vote_cnt;
+    console.log(vote_cnt);
+    myChart.update();
+    Chart = myChart;
+}
+
+
+function Votselect(voteTo) {
+    // labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+    sendMessage(voteTo);
+    // idx = labels.indexOf(voteTo);
+    // Chart.data.datasets[0].data[idx] += 1;
+    // Chart.update();
+    
+}
+
+// send message to backend (python)
+function sendMessage(value) {
+    ws.send(value);
+    event.preventDefault;
+}
+
+ws.onmessage = function(event){
+    var vote_now = JSON.parse(event.data);
+    console.log(vote_now);
+    Chart.data.datasets[0].data = vote_now;
+    Chart.update();
+}
+
+function VotReset(){
+    ws.send("Reset");
+    event.preventDefault;
+}
